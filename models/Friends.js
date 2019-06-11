400cec82416a6f25c2ed0035351f46d16d040ef7
mrhdman <mattrhavins@gmail.com>
2019-06-11 13:36:22 -0400

    working on Friends model

--- models/Friends.js
+++ models/Friends.js
@@ -1,1 +1,17 @@
 const mongoose = require('mongoose');
+
+const FriendsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Friends = mongoose.model('friends', FriendsSchema);
